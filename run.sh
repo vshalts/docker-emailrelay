d2e68e0c3fd3ed17426bbf167d82cf83d3594ded
Vadim Shalts <vshalts@gmail.com>
2016-11-28 15:10:42 +0100

    No need to workaround to run daemon mode, --no-daemon options can be used

--- run.sh
+++ run.sh
@@ -3,11 +3,3 @@
 echo Run emailrelay
 
 /usr/sbin/emailrelay $EMAILRELAY_OPTS
-
-while [ "`pgrep emailrelay`" == "" ]; do sleep 0.1; done
-
-pr=`pgrep emailrelay`
-
-echo Wait PID: $pr to complete
-
-while [ -e /proc/$pr ]; do sleep 0.1; done
